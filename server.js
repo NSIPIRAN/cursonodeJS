b25d487e1726d27c143bb1b42d53930f186f3ebb
Nathaly Sipiran <nsipiran@unitru.edu.pe>
2020-03-05 00:23:42 -0500

    informacion del body fucionando, instalando el body parser

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express')
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 var app= express();
 
+
+app.use( bodyParser.json());
+app.use( bodyParser.urlencoded({extended: false}));
 app.use(router);
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 router.get('/message', function(req,res){
     res.send('Lista de mensajes');
 })
