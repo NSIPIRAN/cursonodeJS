3defb38e4dac6364d5e3f66468ae99aae7aa45d0
Nathaly Sipiran <nsipiran@unitru.edu.pe>
2020-03-04 22:44:59 -0500

    aumentar rutas ¡cómo pueden venir las peticiones?

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const router = express.Router();
 var app= express();
-app.use('/', function(req, res){
-    res.send('hola, este es un servidor serio');
+
+app.use(router);
+
+router.get('/message', function(req,res){
+    res.send('Lista de mensajes');
 })
+
+router.post('/message', function(req,res){
+    res.send('mensajes enviado');
+})
+router.delete('/message', function(req,res){
+    res.send('mensaje eliminado');
+})
+/*app.use('/', function(req, res){
+    res.send('hola, este es un servidor serio');
+})*/
 app.listen(3000);
 console.log('la aplicacion escucha en http://localhost:3000');
