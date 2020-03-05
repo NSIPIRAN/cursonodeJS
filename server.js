23046713ababfb7d04ee79f99251e29ca19555ef
Nathaly Sipiran <nsipiran@unitru.edu.pe>
2020-03-04 22:27:56 -0500

    correcion de seriedad

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var app= express();
 app.use('/', function(req, res){
-    res.send('hola nenes');
+    res.send('hola, este es un servidor serio');
 })
 app.listen(3000);
 console.log('la aplicacion escucha en http://localhost:3000');
