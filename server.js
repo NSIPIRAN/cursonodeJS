1b1044fcd0ca1bac7a67aa836a81e0fab762e0dd
Nathaly Sipiran <nsipiran@unitru.edu.pe>
2020-03-05 11:41:54 -0500

    creacion de network/ para response.js coherente

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const response = require('./network/response')
+
 const router = express.Router();
 var app= express();
 
@@ -9,31 +11,31 @@ app.use( bodyParser.urlencoded({extended: false}));
 app.use(router);
 
 router.get('/message', function(req,res){
-    
     console.log(req.headers)
     //mandar header como respuesta, ver en insomnia en header
     //en custom-header
     res.header({
         "custom-header":"Nuestro valor personalizado"
     })
-    res.send('Lista de mensajes');
+    response.success(req, res,'Lista de mensajes');
 })
 
 router.post('/message', function(req,res){
-    res.send('mensajes enviado');
+   
+    if(req.query.error=="ok"){
+        response.error(req,res,"error de creacion",401)
+    }
+    else{
+        response.success(req, res,'Creado correctamente',201);
+    }
 })
+
 router.delete('/message', function(req,res){
     console.log(req.body);
     console.log(req.query);
-    /*res.send(); //respuesta vacia
-    res.status(201).send(); //respuesta plana 
-    //respuesta estructurada*/
     res.status(201).send(
         {error:'', body:'creado correctamente'});
     
 })
-/*app.use('/', function(req, res){
-    res.send('hola, este es un servidor serio');
-})*/
 app.listen(3000);
 console.log('la aplicacion escucha en http://localhost:3000');
