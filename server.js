04927be268cb584fbf02d9117310e75b65de2f37
Nathaly Sipiran <nsipiran@unitru.edu.pe>
2020-03-05 00:01:36 -0500

    delete para el ignore

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const bodyParser =require('body-parser');
+
 const router = express.Router();
 var app= express();
 
 app.use(router);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 router.get('/message', function(req,res){
     res.send('Lista de mensajes');
 })
